6d29e98c1d1f74c19b7568c37ab27731b2f1477c
Alequin <j-cox03@hotmail.com>
2017-01-26 13:09:58 +0000

    06 complete

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,25 @@
 class Timer
-  #write your code here
+
+  attr_accessor :seconds, :time_string
+
+  def initialize
+    @seconds = 0
+    @time_string = '00:00:00'
+  end
+
+  def seconds=(seconds)
+
+    @seconds = seconds
+
+    hours = seconds / (60*60)
+    remaining_seconds = seconds % (60*60)
+
+    minutes = remaining_seconds / 60
+
+    seconds = remaining_seconds % 60
+
+    @time_string = '%02d:%02d:%02d' % [hours, minutes, seconds]
+
+  end
+
 end
